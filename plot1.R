40ade307e71bdd23c0d97dc919cc460fbec135a2
adnane rahali <rahali.adnane@gmail.com>
2020-09-10 13:36:19 -0500

    modified plot1

--- plot1.R
+++ plot1.R
@@ -14,10 +14,11 @@ total_PM25_by_year <- total_PM25_by_year/1000000
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
 
 #plotting the data
-plot(names(total_PM25_by_year), total_PM25_by_year, xlab = "", ylab = "", 
+plot(total_PM25_by_year, xlab = "", ylab = "", tick = F, labels = "", 
      type = "l", col = "purple", cex.lab = 0.75)
 #customizing the axes
-axis(1, col.axis="dodgerblue")
+axis(1, col.axis="dodgerblue", at = 1:4,
+     labels = c("1999", "2002", "2005", "2008"))
 axis(2, col.axis="dodgerblue")
 #customizing the labels
 mtext("Years", side=1, line=3, col="darkgrey", cex=0.75)
